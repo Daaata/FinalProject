2d837cc484f9bd5bb99b3448f4c7b6326da2fe63
d89012255 <d89012255@gmail.com>
2021-05-29 22:07:53 +0800

    new

--- hello-exprss/try.js
+++ hello-exprss/try.js
@@ -1,25 +1,22 @@
 var express=require('express');
 var app=express();
+const uri = "mongodb+srv://tyson147258:55688@testuse.4dc1h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/test";
 var MongoClient = require('mongodb').MongoClient;
 //Create a database named "mydb":
-var url = "mongodb://localhost:27017/mydb";
-
-
-
 //set view engine
 app.set("view engine","jade")
 //set view directory
 app.set("views","MyViews")
  
 app.get('/',function(req,res){
-    MongoClient.connect("mongodb://localhost:27017/YourDB", {
+    MongoClient.connect("mongodb+srv://tyson147258:55688@testuse.4dc1h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
     
 },function(err, db) {
     if (err) throw err;
-    var dbo = db.db("mydb");
-    dbo.collection("customers").find({}, { projection: { _id: 0, name: 1 } }).toArray(function(err, result) {
+    var dbo = db.db("test");
+    dbo.collection("node").find({}, { projection: { _id: 0, name: 1 } }).toArray(function(err, result) {
     if (err) 
         throw err;
     else
